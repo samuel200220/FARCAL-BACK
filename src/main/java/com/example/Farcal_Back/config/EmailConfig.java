2c921680f26c231e64e93e80bc0ef93212eb59f3
TAGATSING Samuel <samuelftagat@gmail.com>
2025-12-04 12:56:45 +0100

    Corrections des conflits

--- src/main/java/com/example/Farcal_Back/config/EmailConfig.java
+++ src/main/java/com/example/Farcal_Back/config/EmailConfig.java
@@ -23,6 +23,12 @@ public class EmailConfig {
     @Value("${spring.mail.password}")
     private String password;
 
+<<<<<<< HEAD
+=======
+    @Value("${spring.mail.protocol:smtp}")
+    private String protocol;
+
+>>>>>>> 2d88c5e (Correction des conflits)
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -31,6 +37,7 @@ public class EmailConfig {
         mailSender.setPort(port);
         mailSender.setUsername(username);
         mailSender.setPassword(password);
+<<<<<<< HEAD
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
@@ -42,6 +49,31 @@ public class EmailConfig {
         props.put("mail.smtp.writetimeout", "5000");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+=======
+        mailSender.setProtocol(protocol);
+
+        Properties props = mailSender.getJavaMailProperties();
+
+        if (port == 465) {
+            // Configuration SSL (port 465)
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.ssl.enable", "true");
+            props.put("mail.smtp.ssl.trust", host);
+            props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        } else {
+            // Configuration STARTTLS (port 587)
+            props.put("mail.transport.protocol", "smtp");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.starttls.required", "true");
+            props.put("mail.smtp.ssl.trust", host);
+            props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        }
+
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
+>>>>>>> 2d88c5e (Correction des conflits)
         props.put("mail.debug", "false");
 
         return mailSender;
